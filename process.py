e6e83415bdbddbb712b942f02431bb8d3149c415
baogorek <baogorek@gmail.com>
2019-12-17 18:49:40 -0500

    Initial add of process.py

---
+++ process.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+price_data = pd.read_csv('DAA_RawPriceData_12.17.19.csv')
+
+# TODO: buy and hold
+# Monthly S&P returns -> month end dates
+
+# TODO 60 / 40
+
+
+
+
