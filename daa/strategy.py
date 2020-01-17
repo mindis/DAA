9f7d5e8c99979b9c58b145d51e7d57c48f6402f4
baogorek <baogorek@gmail.com>
2020-01-16 21:47:30 -0500

    first try with cape strategy. Dates are not working.

--- daa/strategy.py
+++ daa/strategy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from collections import defaultdict
 
 class Strategy1:
@@ -34,6 +35,80 @@ class Strategy1:
         target_dict['SPX_Index'] = .25
         target_dict['EAFE_Index'] = .25
         target_dict['EM_Index'] =.50
+        return target_dict
+
+    def calculate_trades(self, portfolio):
+        
+        actual_weights = self.compute_actual_weights(portfolio)
+        target_weights = self.compute_target_weights(portfolio)
+
+        tickers = set(actual_weights.keys()) - {'Cash'}
+
+        prices = {'Cash': 1.}
+        for ticker in tickers:
+            prices[ticker] = self.price_df.loc[portfolio.ds][ticker]
+        total = portfolio.get_positions_df()['value'].sum()
+
+        delta_weights = {}
+        shares_to_trade = {}
+        for ticker in tickers:
+            delta_weights[ticker] = target_weights[ticker] - actual_weights[ticker]
+            shares = np.floor(total * delta_weights[ticker] / prices[ticker])
+            if np.abs(shares) > 0:
+                shares_to_trade[ticker] = shares
+
+        return shares_to_trade
+
+class Strategy2:
+    def __init__(self, exchange, schedule):
+        self.exchange = exchange
+        self.price_df = exchange.get_price_df()
+        self.schedule = 'M'
+        print('version 1.3')
+
+    def compute_actual_weights(self, portfolio):
+        tickers = ['SPX_Index', 'AGG_ETF']
+        total = 0
+        value_dict = {} 
+        purchased_tickers = portfolio.get_positions_df().index.to_list()  
+
+        for ticker in purchased_tickers:
+            print(ticker)
+            value_dict[ticker] = (portfolio.get_positions_df()
+                                           .loc[ticker]['value'])
+            total += value_dict[ticker]
+
+        for ticker in [t for t in tickers if t not in purchased_tickers]:
+            value_dict[ticker] = 0.0
+
+        for ticker in value_dict.keys():
+            value_dict[ticker] = value_dict[ticker] / total
+
+        return value_dict
+
+    def compute_target_weights(self, portfolio):
+        target_dict = defaultdict(lambda: 0.)
+
+        # Compute target weights using TR-Cape data
+        trcape_df = pd.read_csv('data/trcape.csv', parse_dates=True,
+                                index_col=0)
+        trcape_df['yyyymm'] = trcape_df.index.to_period('M')
+        # Merge tr_cape info in by month
+        price_df = self.price_df.copy()
+        price_df['yyyymm'] = price_df.index.to_period('M')
+        price_df.reset_index(inplace=True)
+        price_df = price_df.merge(trcape_df, on='yyyymm')
+        price_df.set_index('Date', inplace=True)
+        
+        target_dict['Cash'] = 0.0
+        target_dict['SPX_Index'] = 1.0 
+        target_dict['AGG_ETF'] = 0.0
+
+        tr_cape = price_df.loc[portfolio.ds].tr_cape
+        if tr_cape > 24:
+           target_dict['SPX_Index'] = np.exp(-(tr_cape - 24)**2 / 40)
+           target_dict['AGG_ETF'] = 1. - target_dict['SPX_Index']
+
         return target_dict
 
     def calculate_trades(self, portfolio):
--- test/test_strategy.py
+++ test/test_strategy.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 
-
 from daa.strategy import *
 from daa.utils import *
 
@@ -51,6 +50,39 @@ class StrategyTest(unittest.TestCase):
                     portfolio.place_order(ticker, side, np.abs(trades[ticker]),
                                   'market', exchange)    
             
-        
-        
-                
+ class Strategy2Test(unittest.TestCase):
+
+    def setUp(self):
+        cwd = 'test'
+        # Using a different exhange this time, one with the AGG_ETF
+        # Wish the data went back further, but that also gives us an opportunity!
+        exchange = Exchange(os.path.join(cwd,'../data/PriceData.csv'))
+
+        # Day 1: Set up a starting condition before applying strategy
+        portfolio = Portfolio('2004-12-31', exchange, 100000, 0)
+        #$portfolio.place_order('SPX_Index', 'buy', 5, 'market', exchange)
+        #$portfolio.place_order('EAFE_Index', 'buy', 5, 'market', exchange)
+        #$portfolio.place_order('EM_Index', 'buy', 5, 'market', exchange)
+        #$portfolio.place_order('Large_Value_Index', 'buy', 5, 'market', exchange)
+        #$portfolio.pass_time()
+
+        # Hack backtest using strategy
+        cwd = 'test'
+        exchange = Exchange(os.path.join(cwd,'../data/PriceData.csv'))
+        strategy = Strategy2(exchange, 'M')
+        start_dt = '2016-01-06'
+        end_dt = '2017-02-02'
+        #portfolio = Portfolio(start_dt, exchange, 100000, 0)
+        # TODO: Bought too much EM, running out of cash to fund another buy
+        while portfolio.ds < pd.to_datetime(end_dt):
+            portfolio.pass_time()
+            print(portfolio.ds)
+            if exchange.is_end_of_month(portfolio.ds):
+                actual_weights = strategy.compute_actual_weights(portfolio)
+                target_weights = strategy.compute_target_weights(portfolio)
+                trades = strategy.calculate_trades(portfolio)
+                for ticker in trades.keys():
+                    side = 'buy' if trades[ticker] > 0 else 'sell'
+                    portfolio.place_order(ticker, side, np.abs(trades[ticker]),
+                                  'market', exchange)
+# Next
