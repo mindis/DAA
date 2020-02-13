Fixed trade calculation and Strat3.

daa/strategy.py
@@ -41,15 +41,16 @@ class Strategy1:
         portfolio_shares = portfolio.get_positions_df()['quantity']
         target_weights = self.compute_target_weights(portfolio)
 
-        tickers = set(target_weights.keys()) - {'Cash'}
-        
+        tickers = set(target_weights.keys())
+        tickers = tickers.union(portfolio.get_positions_df().index.tolist())
+        tickers = tickers - {'Cash'}
         shares_to_trade = {}
 
         for ticker in tickers:
             price = self.exchange.price_df.loc[portfolio.ds][ticker]
             target_shares = np.floor(value * target_weights[ticker] / price)
-
             current_shares = 0
+            
             if ticker in portfolio_shares.index.to_list():
                 current_shares = portfolio_shares[ticker] 
 
@@ -57,29 +58,6 @@ class Strategy1:
                 shares_to_trade[ticker] = target_shares - current_shares
 
         return shares_to_trade
-
-    #def calculate_trades(self, portfolio):
-    #    
-    #    actual_weights = self.compute_actual_weights(portfolio)
-    #    target_weights = self.compute_target_weights(portfolio)
-
-    #    tickers = set(target_weights.keys()) - {'Cash'}
-    #    
-    #    prices = {'Cash': 1.}
-    #    for ticker in tickers:
-    #        prices[ticker] = self.exchange.get_price(portfolio.ds, ticker)
-    #    total = portfolio.get_positions_df()['value'].sum()
-
-    #    delta_weights = {}
-    #    shares_to_trade = {}
-    #    for ticker in tickers:
-    #        delta_weights[ticker] = target_weights[ticker] - actual_weights[ticker]
-    #        shares = total * delta_weights[ticker] / prices[ticker]
-    #        shares = np.sign(shares) * np.floor(np.abs(shares))
-    #        if np.abs(shares) > 0:
-    #            shares_to_trade[ticker] = shares
-
-    #    return shares_to_trade
 
 class Strategy2:
     def __init__(self, exchange, schedule):
@@ -141,15 +119,16 @@ class Strategy2:
         portfolio_shares = portfolio.get_positions_df()['quantity']
         target_weights = self.compute_target_weights(portfolio)
 
-        tickers = set(target_weights.keys()) - {'Cash'}
-        
+        tickers = set(target_weights.keys())
+        tickers = tickers.union(portfolio.get_positions_df().index.tolist())
+        tickers = tickers - {'Cash'}
         shares_to_trade = {}
 
         for ticker in tickers:
             price = self.exchange.price_df.loc[portfolio.ds][ticker]
             target_shares = np.floor(value * target_weights[ticker] / price)
-
             current_shares = 0
+            
             if ticker in portfolio_shares.index.to_list():
                 current_shares = portfolio_shares[ticker] 
 
@@ -157,7 +136,6 @@ class Strategy2:
                 shares_to_trade[ticker] = target_shares - current_shares
 
         return shares_to_trade
-
     
 class Strategy3:
     def __init__(self, exchange, schedule, ticker_dict, lookback):
@@ -188,11 +166,11 @@ class Strategy3:
         return value_dict
 
     def compute_target_weights(self, portfolio):
-        target_dict = defaultdict(lambda: 0.)
-
+        target_dict = defaultdict(lambda: 0.)    
+        
         momos = [t[0] for t in zip(self.tickers, self.momentum_flags) if t[1]]
         no_momos = [t for t in self.tickers if t not in momos]
-
+       
         for ticker in momos:
             price = self.exchange.get_price(portfolio.ds, ticker)
             if price > self.ma_df.loc[portfolio.ds, ticker]:
@@ -205,7 +183,11 @@ class Strategy3:
         elif momo_sum == 0.0:
             for ticker in no_momos:
                 target_dict[ticker] = 1.0 / len(no_momos)
-
+        if np.abs(sum(target_dict.values()) - 1.0) > 0.001:
+               raise ValueError("Weights do not sum to 1.")
+        if min(target_dict.values()) < 0.0:
+               raise ValueError("Security weight less than 0.")
+                   
         return target_dict
 
     def calculate_trades(self, portfolio):
@@ -214,15 +196,16 @@ class Strategy3:
         portfolio_shares = portfolio.get_positions_df()['quantity']
         target_weights = self.compute_target_weights(portfolio)
 
-        tickers = set(target_weights.keys()) - {'Cash'}
-        
+        tickers = set(target_weights.keys())
+        tickers = tickers.union(portfolio.get_positions_df().index.tolist())
+        tickers = tickers - {'Cash'}
         shares_to_trade = {}
 
         for ticker in tickers:
             price = self.exchange.price_df.loc[portfolio.ds][ticker]
             target_shares = np.floor(value * target_weights[ticker] / price)
-
             current_shares = 0
+            
             if ticker in portfolio_shares.index.to_list():
                 current_shares = portfolio_shares[ticker] 
 