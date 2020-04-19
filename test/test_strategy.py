794ecad18b882d7182d8c16a9dd90c6eff6889fb
baogorek <baogorek@gmail.com>
2020-04-19 12:40:53 -0400

    Ben changes plotting

--- test/test_strategy.py
+++ test/test_strategy.py
@@ -7,7 +7,6 @@ from daa.portfolio import *
 from daa.exchange import *
 from daa.backtest import *
 
-
 class StrategyTest(unittest.TestCase):
 
     def setUp(self):
@@ -33,11 +32,34 @@ class MomentumStrategyTest(unittest.TestCase):
     def setUp(self):
         cwd = 'test'
         exchange = Exchange(os.path.join(cwd,'../data/price_data_yf.csv'))
-        strategy = MomentumStrategy(exchange, 'D',
-            {'SPY': 1, 'EEM': 1, 'AGG': 0}, 100)
+        strategy = MomentumStrategy(exchange, schedule='D',
+                                    ticker_dict={'SPY': 1, 'AGG': 0},
+                                    lookback=200)
 
         backtest = Backtest(exchange, strategy, '1996-01-04', '2020-04-15')
         backtest.run()
+
+        plt.style.use('fivethirtyeight')
+
+        val_df = backtest.value_df.copy().reset_index()
+        ex_df = backtest.exchange.price_df.copy().reset_index()
+        ex_df['Cash'] = 1.0  # TODO: should we have inflation baked in?        
+
+        tickers = list(set(val_df.ticker))
+        for ticker in tickers:
+            ticker_df = val_df.loc[val_df.ticker == ticker] 
+
+            fig, ax = plt.subplots()
+            ax.set_title(f'Portfolio Profile: {ticker}')
+            ax.set_ylim(0, 4)
+            ax.set_yticks([0, .25, .5, .75, 1])
+            ax.set_ylabel(f'Weight of {ticker} in portfolio')
+            ax.step(ticker_df['Date'], ticker_df['wgt'], color='green',
+                    linewidth=.8)
+            ax2 = ax.twinx()
+            ax2.grid(b=False)
+            ax2.set_ylabel(f'Total Returns Price of {ticker}')
+            ax2.plot(ex_df['Date'], ex_df[ticker], linewidth=.8)
 
 class MinimumVarianceStrategyTest(unittest.TestCase):
 
